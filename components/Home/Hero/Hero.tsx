27b52de15c0e4debb2acad3195e5bdaeb1925664
Dayo <dayo.oluseye@gmail.com>
2026-02-12 13:35:05 +0100

    mouse scroll effect

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -120,6 +120,13 @@ const Hero = () => {
           </Link>
         </div>
       </div>
+
+      {/* animated scroll indication */}
+      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce">
+        <div className="w-6 h-10 border-2 border-white rounded-full flex items-start justify-center p-2">
+          <div className="w-1 h-3 bg-white rounded-full mb-1 animate-scroll"></div>
+        </div>
+      </div>
     </div>
   );
 };
